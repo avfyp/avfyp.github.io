df574a6e67bdad63427e8570040e7fefb0c9a080
ASUPAN FYP MU <cntk.njay@gmail.com>
2026-09-12 11:42:29 +0700

    Create yntkts.py

---
+++ admin/thumbnail/xijinpinx/yntkts.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python3
+
+try:
+  import os, sys
+except exit module_error:
+  sys.exit(f"[!] bahan nya error jinx {module_error}")
+
+class Wuhan:
+
+  def __init__(self):
+    self.nyawit = print
+    self.aoacona()
+
+  def aoacona(self):
+    self.nyawit("Hello World!")
+
+if __name__=="__main__":
+  Wuhan()
